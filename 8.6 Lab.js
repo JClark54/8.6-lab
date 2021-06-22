a9426813ef5aab9988918f9aa0030a8cedd9ea32
JClark54 <85198081+JClark54@users.noreply.github.com>
2021-06-21 19:04:59 -0500

    Add files via upload

---
+++ 8.6 Lab.js
@@ -0,0 +1,18 @@
+
+8.6 Lab
+'use strict'
+for (let countdown = 10; countdown > 0; countdown--){
+  console.log("COUNTDOWN : " + countdown)
+
+  for (let countdown= 10; countdown> 0; countdown--){
+  console.log("countdown : " + countdown);
+  }
+
+  for (let numOfLoops = 0; numOfLoops <  12; numOfLoops++){
+    console.log("Loop has ran  : " + numOfLoops + " times.");
+  }
+
+  do  while (loopCounter <99){
+    loopCounter ++;
+    console.log(loopCounter);
+  }
